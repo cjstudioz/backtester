46f6c570f9ddc203b67068cdb5b9fda23b73b0bf
CJ <cjl>
2018-11-14 21:38:29 +0000

    replaced expire with settleOption

--- portfolio.py
+++ portfolio.py
@@ -12,6 +12,12 @@ class Portfolio():
         self.options.setdefault(object.toTuple(), object).amount += amount
         self.trades.append([str(object), amount, price])
 
-    def expireOptions(self):
+    def settleOptions(self):
+        settlement = 0
         expiredOptions = [v for v in self.options.values() if v.isExpired()]
-        self.options = {k: v for k, v in self.options.iteritems() if not v.isExpired()}
+
+        if expiredOptions:
+            settlement = sum(option.moniness for option in expiredOptions)
+            self.options = {k: v for k, v in self.options.iteritems() if not v.isExpired()}
+
+        return settlement
