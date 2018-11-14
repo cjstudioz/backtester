973dee10bae123f6f84aa70d884b016014c54383
CJ <cjl>
2018-11-14 15:17:34 +0000

    added greeks

--- option.py
+++ option.py
@@ -3,7 +3,7 @@ import scipy.stats as si
 from functools import lru_cache
 
 OPTION_TYPE_CALL = 1
-OPTION_TYPE_PUT = 0
+OPTION_TYPE_PUT = -1
 DAYS_IN_YEAR = 360
 
 @lru_cache()
@@ -16,20 +16,43 @@ def _d2(S, K, T, r, sigma):
     return (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
 
 @lru_cache()
-def callPrice(S, K, T, r, sigma):
+def price(S, K, T, r, sigma, putcall):
     d1 = _d1(S, K, T, r, sigma)
     d2 = _d2(S, K, T, r, sigma)
-    res = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    res = (S * si.norm.cdf(putcall * d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(putcall * d2, 0.0, 1.0))
+    return putcall * res
+
+@lru_cache()
+def delta(S, K, T, r, sigma, putcall):
+    d1 = _d1(S, K, T, r, sigma)
+    delta_call = putcall * si.norm.cdf(putcall * d1, 0.0, 1.0)
+    return delta_call
+
+@lru_cache()
+def _prob_density(S, K, T, r, sigma):
+    d1 = _d1(S, K, T, r, sigma)
+    res = 1 / np.sqrt(2 * np.pi) * np.exp(-d1 ** 2 * 0.5)
     return res
 
 @lru_cache()
-def putPrice(S, K, T, r, sigma):
-    d1 = _d1(S, K, T, r, sigma)
-    d2 = _d2(S, K, T, r, sigma)
-    res = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+def gamma(S, K, T, r, sigma):
+    prob_density = _prob_density(S, K, T, r, sigma)
+    gamma = prob_density / (S * sigma * np.sqrt(T))
+    return gamma
+
+@lru_cache()
+def vega(S, K, T, r, sigma):
+    prob_density = _prob_density(S, K, T, r, sigma)
+    res = S * prob_density * np.sqrt(T)
     return res
 
-
+@lru_cache()
+def theta(S, K, T, r, sigma, putcall):
+    d1 = _d1(S, K, T, r, sigma)
+    d2 = _d2(S, K, T, r, sigma)
+    prob_density = _prob_density(S, K, T, r, sigma)
+    res = (-sigma * S * prob_density) / (2 * np.sqrt(T)) - putcall * r * K * np.exp(-r * T) * si.norm.cdf(putcall * d2, 0.0, 1.0)
+    return res
 
 class Option():
     def __init__(self, underlying, spot, strike, maturity, rate, impliedVol, optionType, context):
@@ -46,12 +69,14 @@ class Option():
             raise RuntimeError('Option is expired! marutity:{}, context.date:{}'.format(self.maturity, self.context.date))
 
         timeToMaturityRaw = self.maturity - self.context.date
-        return [self.spot, self.strike, timeToMaturityRaw.days/DAYS_IN_YEAR, self.rate, self.impliedVol]
+        return [self.spot, self.strike, timeToMaturityRaw.days/DAYS_IN_YEAR, self.rate, self.impliedVol, self.optionType]
 
     def price(self):
-        args = self._getPricingArgs()
-        res = callPrice(*args) if self.optionType == OPTION_TYPE_CALL else putPrice(*args)
-        return res
+        return price(*self._getPricingArgs())
+
+    def delta(self):
+        deltaRaw = delta(*self._getPricingArgs())
+
 
     def isExpired(self):
         return self.context.date >= self.maturity
@@ -61,7 +86,8 @@ if __name__ == '__main__':
 
     context = type('test', (object,), {})() #TODO: replace with real context
     context.date = date.today()
-    call = Option('SPY', 100, 101, date.today() + timedelta(days=5), 0.01, 0.1, OPTION_TYPE_CALL, context)
-    put = Option('SPY', 100, 101, date.today() + timedelta(days=5), 0.01, 0.1, OPTION_TYPE_PUT, context)
-    print(call, call.price())
-    print(put, put.price())
+
+    call = Option('SPY', 100, 1, date.today() + timedelta(days=5), 0.01, 0.1, OPTION_TYPE_CALL, context)
+    put = Option('SPY', 1, 101, date.today() + timedelta(days=1), 0.01, 0.1, OPTION_TYPE_PUT, context)
+    print(call, call.price(), call.delta())
+    print(put, put.price(), put.delta())
