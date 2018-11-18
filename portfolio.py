badcc52c31ff2324f29624a48815f71dd0b9788f
cj <cjstudios@gmail.com>
2018-11-18 00:52:43 +0000

    use dataframe in portfolio

--- portfolio.py
+++ portfolio.py
@@ -1,30 +1,52 @@
 import pandas as pd
+from option_pricer import delta, OPTION_TYPE_CALL, OPTION_TYPE_PUT
+
+INDEX = ['Underlying', 'Strike', 'Maturity', 'PutCall']
+POSITION_COLS = ['Amount']
+#TRANSACTION_COLS = INDEX
 
 class Portfolio():
     def __init(self, context):
         self.context = context
-        self.options = {}
-        self.stock = {}
-        self.trades = pd
+        self.dfPositions = pd.DataFrame(columns=INDEX+COLUMNS).set_index(INDEX)
 
-    def tradeOption(self, option: tuple, underlying: str, price: double, amount: double):
-        self.options.setdefault(
-            underlying, {}
-        ).setdefault(option, 0) += amount
 
-        self.options.QUERY
+    def executeTrade(self,
+            underlying: str,
+            strike: double,
+            maturity: date,
+            putcall: int, #option_pricer.OPTION_TYPE_CALL  OPTION_TYPE_PUT
+            price: double,
+            amount: double,
+    ):
 
-        #self.trades.append(*(option + (underlying, price, amount)))
+        index = (underlying, strike, maturity, putcall)
+        self.dfPositions.loc[index, 'Amount'] = \
+            self.dfPositions.loc[index, 'Amount'] + amount \
+        if index in self.dfPositions \
+            else amount
 
-    def delta(underlying):
+        self.context.balance -= price * amount
+
+        # self.dfTransactions.append([
+        #     [self.context.date, underlying, strike, maturity, price, amount]
+        # ])
+
+
+
+
+    def delta(self, underlying:str):
         ctx = self.context
-        sum(delta(
+        deltaRaw = delta(
             ctx.spot(underlying),
-            option.strike,
-            (option.maturity - ctx.date).days / ctx.DAYS_IN_YEAR,
+            self.dfPositions['Strike'],
+            (self.dfPositions['Maturity'] - ctx.date).days / ctx.DAYS_IN_YEAR,
             ctx.rate,
-            ctx.vol(underlying)
-        ) for option in self.options[underlying].values())
+            ctx.vol(underlying),
+            self.dfPositions['PutCall']
+        )
+
+        return deltaRaw.sum()
 
     def settleOptions(self):
         settlement = 0
