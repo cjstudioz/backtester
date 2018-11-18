7c9b665c0c75c7e53e3a3aa0aed09256dd276732
CJ <cjl>
2018-11-18 00:27:22 +0000

    first commit

--- context.py
+++ context.py
@@ -1,27 +1,33 @@
 import pandas as pd
+import numpy as np
 from datetime import date
 
-class Context():
+class Context:
     def __init__(self, mktdata: pd.DataFrame, startdate: date=None):
         self.mktdata = mktdata #TODO: pass separate mktdata in for spot so no dupes
-        self.date = startdate or np.min(mktdata['date'])
+        self.date = startdate or np.min(mktdata['Date'])
 
     def spot(self, symbol: str): #TODO: ideally should be able to call price on any asset class
         """
 
         """
-        return self.mktdata.query(f"""
-            Date == {self.date}                                  
-            & Symbol == {symbol}            
-        """).at[0, 'Spot']
+        md = self.mktdata
+        res = md[
+            (md['Date'] == self.pd.Timestamp(self.date)) &
+            (md['Symbol'] == symbol)
+        ].reset_index()
+        return res.at[0, 'Spot']
 
-    def atmvol(self, symbol: str): #TODO: ideally should be able to call price on any asset class
+
+    def vol(self, symbol: str, maturity: date): #TODO: ideally should be able to call price on any asset class
         """
 
         TODO: interpolate??
         """
-        return self.mktdata.query(f"""
-            Date == {self.date}                                  
-            & Symbol == {symbol}
-            & Symbol == {symbol}            
-        """).at[0, 'Volatility']
+        md = self.mktdata
+        res = md[
+            (md['Date'] == pd.Timestamp(self.date)) &
+            (md['Symbol'] == symbol) &
+            (md['Maturity'] == pd.Timestamp(maturity))
+        ].reset_index()
+        return res.at[0, 'Volatility']
