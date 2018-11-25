2a257a57d319c230e589fca203a33bbd0b687eb9
CJ <cjl>
2018-11-25 23:49:47 +0000

    add comments and TODOs

--- backtester/pricers/black_scholes_option_pricer.py
+++ backtester/pricers/black_scholes_option_pricer.py
@@ -12,6 +12,19 @@ def _d2(S, K, T, r, sigma):
     return (log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
 
 def price(S, K, T, r, sigma, putcall, amount=1):
+    """
+
+    option price
+
+    :param S: Spot
+    :param K:  Strike
+    :param T: time to maturity in years #TODO: change func signatures to take T in days instead of years
+    :param r: interest rate 0.01 = 1%/annum
+    :param sigma: implied vol / annum
+    :param putcall: 1 call, -1 put see backtester.pricer.option_types
+    :param amount: optional position quantity multiplier
+    :return:
+    """
     d1 = _d1(S, K, T, r, sigma)
     d2 = _d2(S, K, T, r, sigma)
     res = (S * norm.cdf(putcall * d1, 0.0, 1.0) - K * exp(-r * T) * norm.cdf(putcall * d2, 0.0, 1.0))
@@ -35,6 +48,9 @@ def vega(S, K, T, r, sigma, _putcall_unused, amount=1):
     return res * amount
 
 def theta(S, K, T, r, sigma, putcall, amount=1):
+    """
+    :return: Theta per day (instead of year)
+    """
     d1 = _d1(S, K, T, r, sigma)
     d2 = _d2(S, K, T, r, sigma)    
     res = (-sigma * S * norm.pdf(d1)) / (2 * sqrt(T)) - putcall * r * K * exp(-r * T) * norm.cdf(putcall * d2)
